7a913008d7c8cf9e806b3c4ff02a5bd41fe0c565
jacks-pp <jackson.neves@picpay.com>
2023-07-30 17:41:12 -0300

    added bulma framework

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -35,7 +35,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             },
             {
                 test: /\.m?js$/,
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -29,7 +29,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             },
             {
                 test: /\.m?js$/,
